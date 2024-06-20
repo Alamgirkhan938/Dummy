3217ea3dc8230e7079b1dcf86cda83bf1f119f06
Alamgir khan <alamgirkhanmarwat6@gmail.com>
2024-06-19 22:32:28 -0700

    Dummy
    dummy

---
+++ lib/homescreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [Text('alamgir')],),
+    );
+  }
+}
